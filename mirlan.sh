d6f22a52b305ea073cc8bc0dd7302c1115b3abbd
Mirlanbekk <pirmatov@gmail.com>
2022-06-16 21:55:49 -0700

    basic bash script

---
+++ mirlan.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir /mnt/c/Users/pirma/OneDrive/Desktop/bash_script/Bash_Scripts/test2
+
+echo "What is your name?"
+
+read name
+
+echo "How old are you?"
+
+read age
+
+echo "Hello $name, you are $age years old."
+
+
+
