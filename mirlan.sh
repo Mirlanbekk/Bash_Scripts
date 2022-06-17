ed5a651944e4ac43a4092478eadc248dd000d4e6
Mirlanbekk <pirmatov@gmail.com>
2022-06-16 22:09:32 -0700

    basic ramdom bash script

--- mirlan.sh
+++ mirlan.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-
+# author: Mirlan
+# This random bash script.
 
 echo "What is your name?"
 
@@ -11,6 +12,12 @@ echo "How old are you?"
 read age
 
 echo "Hello $name, you are $age years old."
+
+sleep 2
+
+iwillberich=$((( $RANDOM % 15 ) + $age ))
+
+echo "$name, you will become a millionara when you are $iwillberich years old"
 
 
 
