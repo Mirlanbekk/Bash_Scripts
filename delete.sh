14709077c8df66a46a8be732745e02ef228ba2ef
Mirlanbekk <pirmatov@gmail.com>
2022-06-16 22:26:11 -0700

    Delete directory with bash script from local repositori

--- delete.sh
+++ delete.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# author: Mirlan
+# I CREATE BASH SCRIPT TO DELETE WORK DIREKTORY FROM LOCAL.
+
+
+
 rm -rf /mnt/c/Users/pirma/OneDrive/Desktop/bash_script/Bash_Scripts/work
 
 sleep 2
